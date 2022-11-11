a194cfd1ac43c371bba1d08aafebc2ed625fb347
JackSSads <fotosjackson2018@hotmail.com>
2022-11-11 00:16:43 -0300

    Feature: Corrigindo CSS e Link da imagem

--- src/components/Salutation/Salutation.js
+++ src/components/Salutation/Salutation.js
@@ -7,7 +7,7 @@ const Salutation = () => {
 
             <div className="text-left w-1/3">
 
-                <h2 class="text-5xl font-bold">
+                <h2 class="text-4xl font-bold">
                     Seja bem-vindo(a) ao meu humilde espaço!
                 </h2>
 
@@ -16,15 +16,15 @@ const Salutation = () => {
                 <a type="button" href="https://wa.me/5584994015772" target="_blanck" className="
                     flex justify-center items-center
                     p-5 mt-10 bg-blue-500 h-1 hover:bg-blue-600
-                    rounded-lg text-white
-                    transition-all duration-200 delay-100">
+                    rounded-lg text-white shadow-xl shadow-blue-500
+                    transition-all duration-200 delay-100 animate-bounce">
                     Fazer orçamento
                 </a>
 
             </div>
             
             <img 
-            src="image1.jpg" 
+            src="img/image1.jpg" 
             className="h-96 rounded-lg" />
         </div>
     );
