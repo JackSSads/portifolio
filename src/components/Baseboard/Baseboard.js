71eabcc511271e21878ebb0b0cd30a126a3a99b2
JackSSads <fotosjackson2018@hotmail.com>
2022-11-11 00:15:21 -0300

    Feature: criando Baseboard

---
+++ src/components/Baseboard/Baseboard.js
@@ -0,0 +1,18 @@
+const Baseboard = () => {
+    return (
+        <div className="
+            bg-gray-500 h-60 w-full bg-cover bg-center
+            relative flex justify-start items-center">
+
+            <img src="img/image3.jpg"
+                className="h-full w-full object-cover
+                absolute mix-blend-overlay"/>
+
+            <h3 className="ml-10 text-5xl text-white font-semibold">
+                Vamos Criar o eu Site Já!</h3>
+
+        </div>
+    );
+};
+
+export default Baseboard;
