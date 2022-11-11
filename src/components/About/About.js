de803678d68570fe1c4e5a080fc4eb895f1ac9eb
JackSSads <fotosjackson2018@hotmail.com>
2022-11-10 21:52:08 -0300

    Feature: Adicionando tag para imagem

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 const About = () => {
     return (
-        <div id="about" className="flex flex-col justify-center mt-80 h-full">
+        <div id="about" 
+        className="flex flex-col justify-center mt-80 h-full">
 
             <div className="text-left">
                 <p className="font-light text-lg">Sobre</p>
@@ -8,11 +9,12 @@ const About = () => {
                     text-blue-500 font-semibold text-2xl
                     mb-16">Saiba um pouco sobre mim</h3>
             </div>
-            <div className="flex flex-row justify-between">
+            <div className="flex flex-row justify-around">
 
-                <div className="
-                bg-slate-600 w-1/3 h-96 rounded-lg">
-                </div>
+                <img src="my.jpg" 
+                alt="" 
+                className="max-h-96 rounded-xl"
+                />
 
                 <div className="w-1/2 flex justify-center items-center">
                     <p>
