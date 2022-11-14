3280f9f06e2bdd92b0523ee487db46a6e27f1224
JackSSads <fotosjackson2018@hotmail.com>
2022-11-14 01:42:35 -0300

    Feature: atualizando textos

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,7 @@
 const About = () => {
     return (
         <div id="about" 
-            className="flex flex-col justify-center mt-80 h-full">
+            className="flex flex-col justify-center mt-32 h-full">
 
             <div className="text-left">
                 <p className="font-light text-lg">Sobre</p>
@@ -19,34 +19,42 @@ const About = () => {
                 />
 
                 <div className="
-                    w-full flex justify-center items-center mt-10
+                    w-full flex flex-col justify-center items-center 
+                    mt-10 text-start text-gray-500
                     md:w-1/2">
                     <p>
-                        Lorem ipsum dolor sit
-                        amet consectetur adipisicing
-                        elit. Aliquid sapiente rem
-                        sunt velit, cumque et veritatis
-                        fugiat atque porro eveniet
-                        cum repellendus exercitationem,
-                        recusandae officia! Ut deserunt
-                        tempora cumque illum!
-                        Lorem ipsum dolor sit
-                        amet consectetur adipisicing
-                        elit. Aliquid sapiente rem
-                        sunt velit, cumque et veritatis
-                        fugiat atque porro eveniet
-                        cum repellendus exercitationem,
-                        recusandae officia! Ut deserunt
-                        tempora cumque illum!
-                        Lorem ipsum dolor sit
-                        amet consectetur adipisicing
-                        elit. Aliquid sapiente rem
-                        sunt velit, cumque et veritatis
-                        fugiat atque porro eveniet
-                        cum repellendus exercitationem,
-                        recusandae officia! Ut deserunt
-                        tempora cumque illum!
+                        olá! Meu nome é <span className="underline">Jackson Souza</span>, tenho 22 anos,
+                        sou desenvolvedor Fullstack Web Js. Atuo na 
+                        área a pouco mais de um ano como freelancer.
+                        Estou me reinventando a cada dia, sempre procurando
+                        melhorar e focado no meu objetivo: fazer o melhor!
+                        Aplico meus conhecimento em projetos pessoais e proficionais.
                     </p>
+                    <p className="my-5">
+                        No decorrer da minha carreira, adquiri conhecimento em cursos como:
+                    </p>
+                    <p>
+                        • Curso JavaScript
+                    </p>
+                    <p>
+                        • Curso Node Js
+                    </p>
+                    <p>
+                        • Curso HTML5, CSS3 & Bootstrap5
+                    </p>
+                    <p>
+                        • Curso React Js
+                    </p>
+                    <p>
+                        • Curso Next Js
+                    </p>
+                    <p>
+                        • Curso Nest Js
+                    </p>
+
+                    <p className="mt-5">
+                        Também sou bacharel em Analise e Desenvolvimento de Sistemas.
+                    </p>                    
                 </div>
             </div>
 
