6bea295b0a2307b8cc190d87b5a7f8c6f467853e
JackSSads <fotosjackson2018@hotmail.com>
2022-11-18 16:34:35 -0300

    Feature: Adicionando carrosel de itens

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,8 @@
-import { SiJavascript, SiNestjs } from "react-icons/si";
-import { TbBrandNextjs } from "react-icons/tb";
+import Carrossel from "../Carrossel/Carrossel";
 
 const Skills = () => {
     return (
-        <div className="
+        <div id="elemento" className="
             flex flex-row items-center justify-between
             mt-32 h-full">
 
@@ -13,37 +12,15 @@ const Skills = () => {
                 <p className="font-light text-lg mb-1">
                     Habilidades
                 </p>
-        
+
                 <h2 className="text-start font-bold uppercase">Meu objetivo é desenvolver as
-                    <span className="
-                    text-blue-500 uppercase font-bold"> Melhores </span>
+                    <span className="text-blue-500 uppercase font-bold"> Melhores </span>
                     aplicações Web
                 </h2>
 
-                <div className="
-                    flex flex-row justify-between
-                    w-full h-3/4 mt-10">
-
-                    <div className="
-                        h-32 mr-1 p-2 flex flex-col justify-around items-center
-                        mt-16 rounded-lg bg-blue-500 
-                        text-white font-bold text-6xl md:h-44 w-44">
-                        <SiJavascript></SiJavascript></div>
-
-                    <div className="
-                        h-32 mr-1 p-2 flex flex-col justify-around items-center
-                        mt-16 rounded-lg bg-blue-500 
-                        text-white font-bold text-6xl md:h-44 w-44">
-                        <TbBrandNextjs></TbBrandNextjs></div>
-
-                    <div className="
-                        h-32 mr-1 p-2 flex flex-col justify-around items-center
-                        mt-16 rounded-lg bg-blue-500 
-                        text-white font-bold text-6xl md:h-44 w-44">
-                        <SiNestjs></SiNestjs></div>
-
-                </div>
+                <Carrossel />
             </div>
+
         </div>
     );
 };
