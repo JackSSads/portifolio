348d20bcc572808e185da2a59e99d6f7e4ec61c0
JackSSads <fotosjackson2018@hotmail.com>
2022-11-18 16:45:49 -0300

    Feature: corrigindo design

--- src/components/Salutation/Salutation.js
+++ src/components/Salutation/Salutation.js
@@ -9,19 +9,20 @@ const Salutation = () => {
                 text-left w-full 
                 md:w-1/3">
 
-                <h2 class="text-left text-3xl sm:text-5xl font-bold mt-10 md:">
+                <h2 class="text-left text-3xl font-bold sm:text-5xl mt-10">
                     Seja bem-vindo(a) ao meu humilde espaço!
                 </h2>
 
                 <p className="">Quero que meus clientes se sintam incríveis e únicos. Levando seus projetos a outro nível!</p>
 
-                <a type="button" href="https://wa.me/5584994015772" rel="" className="
-                    flex justify-center items-center 
-                    w-full h-1 p-5 mt-10 text-white bg-blue-500 rounded-lg md:w-2/3
-                    shadow-lg  shadow-blue-300 dark:shadow-md dark:shadow-blue-400
-                    hover:bg-blue-600transition-all duration-200 delay-100 animate-bounce
-                    ">
-                    Fazer orçamento
+                <a type="button" href="https://wa.me/5584994015772" rel="" 
+                    className="
+                        flex justify-center items-center font-bold
+                        w-full h-1 p-5 mt-10 text-white bg-blue-500 rounded-lg md:w-2/3
+                        shadow-lg  shadow-blue-300 dark:shadow-md dark:shadow-blue-400
+                        hover:bg-blue-600 md:transition-all md:duration-100 md:delay-100 md:animate-bounce
+                        ">
+                    Fazer Orçamento
                 </a>
             </div>
             
