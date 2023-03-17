8748cb2cfa73d3faf82a5cafaa02072d1e824c3d
JackSSads <fotosjackson2018@hotmail.com>
2023-03-17 20:08:18 -0300

    feature: corrigindo responcividade

--- src/components/Baseboard/Baseboard.js
+++ src/components/Baseboard/Baseboard.js
@@ -10,7 +10,7 @@ const Baseboard = () => {
                 className="h-full w-full object-cover
                 absolute mix-blend-overlay"/>
 
-            <h3 className="ml-10 text-5xl shadow-gray-700 shadow-2xl
+            <h3 className="mx-10 text-5xl shadow-gray-700 shadow-2xl
                 text-black font-semibold bg-blue-200 rounded-2xl p-5
                 mix-blend-screen">
                 Venha Criar seu Site!</h3>
