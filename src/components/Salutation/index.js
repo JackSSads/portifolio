d003aac7ab9c3ec1a0361c5a2552cafa5eb34ade
JackSSads <fotosjackson2018@hotmail.com>
2023-05-12 17:27:06 -0300

    ref: atualização de responsividade

--- src/components/Salutation/index.js
+++ src/components/Salutation/index.js
@@ -1,21 +1,22 @@
+import { TextWriter } from "../textWriter";
 
 const Salutation = () => {
     return (
         <div className="
-            flex flex-col items-center
-            h-full md:flex-row md:justify-between ">
+                flex flex-col items-center  
+                h-full md:h-[432px] md:flex-row md:justify-between">
 
             <div className="
-                text-left w-full 
-                md:w-1/3">
+                     text-left w-full h-full
+                     md:w-1/3
+                     flex flex-col justify-evenly lg:justify-center">
 
-                <h2 class="text-left text-3xl font-bold sm:text-5xl mt-10">
-                    Seja bem-vindo(a) ao meu humilde espaço!
-                </h2>
+                <TextWriter
+                    text1={"Seja bem-vindo(a) ao meu humilde espaço!"}
+                    text2={"Quero que meus clientes se sintam incríveis e únicos. Levando seus projetos a outro nível!"} />
 
-                <p className="">Quero que meus clientes se sintam incríveis e únicos. Levando seus projetos a outro nível!</p>
-
-                <a type="button" href="https://wa.me/5584994015772?text=Ol%C3%A1%20Gostaria%20de%20um%20or%C3%A7amento..." rel="" 
+                <a type="button"
+                    href="https://wa.me/5584994015772?text=Ol%C3%A1%20Gostaria%20de%20um%20or%C3%A7amento..." rel=""
                     className="
                         flex justify-center items-center font-bold
                         w-full h-1 p-5 mt-10 text-white bg-blue-500 rounded-lg md:w-2/3
@@ -25,8 +26,8 @@ const Salutation = () => {
                     Fazer Orçamento
                 </a>
             </div>
-            
-            <img 
+
+            <img
                 src="img/image1.jpg" alt=""
                 className="h-full rounded-lg mt-10 
                     md:h-72 lg:h-80 xl:h-96" />
