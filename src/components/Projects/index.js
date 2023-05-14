16c9edc95517940d117e93421321707874647c52
JackSSads <fotosjackson2018@hotmail.com>
2023-05-14 19:19:43 -0300

    ref: ocultando projetos

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,7 +4,7 @@ import { CarroselProjects } from "../CarroselProjects";
 
 const Projects = () => {
     return (
-        <div className="mt-32 h-full">
+        <div className="mt-32 h-full hidden">
 
             <div className="text-left">
                 <p className="text-gray-600 text-24 mb-1">
