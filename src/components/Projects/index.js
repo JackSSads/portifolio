2f097f030490e9124bfcdc634453cb717bacd143
JackSSads <fotosjackson2018@hotmail.com>
2023-05-14 18:59:00 -0300

    feat: iniciando criação do componente

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,20 +1,25 @@
 /* Trabalho futuro de implementação dos meus projetos */
 
+import { CarroselProjects } from "../CarroselProjects";
+
 const Projects = () => {
     return (
-        <div className="hidden
-        /* flex flex-row justify-between items-center
-        mt-32 h-full */">
+        <div className="mt-32 h-full">
 
             <div className="text-left">
                 <p className="text-gray-600 text-24 mb-1">
                     Meus Projetos
                 </p>
                 <h2 className="
-                    text-blue-500 font-semibold text-2xl
-                    mb-16">
-                    Veja alguns projetos desenvolvidos
+                    text-blue-500 font-semibold text-2xl mb-16">
+                    Esses são os meu principais projetos desenvolvidos
                 </h2>
+            </div>
+            <div className="
+            w-full 
+            h-52 sm:h-[380px] md:h-[450px]
+            rounded-md flex justify-center">
+                <CarroselProjects />
             </div>
         </div>
     );
